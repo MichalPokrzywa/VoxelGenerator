a21fa6b807ade4657f8215cb614e1cbec0d0b2b0
MichalPokrzywa <michpok467@student.polsl.pl>
2023-11-12 18:10:46 +0100

    First Experiment Part 1

---
+++ Assets/scripts/Expiremnt1/BlockType.cs
@@ -0,0 +1,15 @@
+public enum BlockType{
+
+    Nothing,
+    Air,
+    Grass_Dirt,
+    Dirt,
+    Grass_stone,
+    Stone,
+    TreeTrunk,
+    TreeLeafesTransparent,
+    TreeLeafsSolid,
+    Water,
+    Sand
+
+}
---
+++ Assets/scripts/Expiremnt1/Chunk.cs
@@ -0,0 +1,103 @@
+using System;
+using UnityEngine;
+
+public static class Chunk
+{
+    public static void LoopThroughTheBlocks(ChunkData chunkData, Action<int, int, int> actionPerform)
+    {
+        for(int index = 0; index < chunkData.blocks.Length; index++)
+        {
+            var position = GetPosiitonFromIndex(chunkData, index);
+            actionPerform(position.x, position.y, position.z);
+        }
+    }
+    //convert a 1d array index to a 3d arrayindex
+    static Vector3Int GetPosiitonFromIndex(ChunkData chunkData, int index)
+    {
+        int x = index % chunkData.chunkSize;
+        int y = (index / chunkData.chunkSize) % chunkData.chunkHeight;
+        int z = index / (chunkData.chunkSize * chunkData.chunkHeight);
+        return new Vector3Int(x, y, z);
+    }
+    static int GetIndexFromPosition(ChunkData chunkData, int localPositionX, int localPositionY, int localPositionZ)
+    {
+        return localPositionX + chunkData.chunkSize * localPositionY + chunkData.chunkHeight * chunkData.chunkSize * localPositionZ;
+    }
+    //in chunk coordinates
+    static bool InRange(ChunkData chunkData, int axisCoordinate)
+    {
+        if(axisCoordinate < 0 || axisCoordinate >= chunkData.chunkSize)
+            return false;
+        return true;
+    }
+
+    static bool InRangeHeight(ChunkData chunkData, int yCoordinates)
+    {
+        if (yCoordinates < 0 || yCoordinates >= chunkData.chunkHeight)
+            return false;
+        return true;
+    }
+
+    public static void SetBlock(ChunkData chunkData, Vector3Int localPosition, BlockType block)
+    {
+        if (InRange(chunkData, localPosition.x) && InRangeHeight(chunkData, localPosition.y) &&
+            InRange(chunkData, localPosition.z))
+        {
+            int index = GetIndexFromPosition(chunkData, localPosition.x,localPosition.y,localPosition.z);
+            chunkData.blocks[index] = block;
+        }
+        else
+        {
+            throw new Exception("Need to ask World for appropiate chunk");
+        }
+    }
+
+    public static BlockType GetBlockFromChunkCoordinates(ChunkData chunkData, Vector3Int chunkCoordinates)
+    {
+        return GetBlockFromChunkCoordinates(chunkData, chunkCoordinates.x, chunkCoordinates.y, chunkCoordinates.z);
+    }
+    public static BlockType GetBlockFromChunkCoordinates(ChunkData chunkData, int x, int y, int z)
+    {
+        if (InRange(chunkData, x) && InRangeHeight(chunkData, y) && InRange(chunkData, z))
+        {
+            int index = GetIndexFromPosition(chunkData, x, y, z);
+            return chunkData.blocks[index];
+        }
+
+        return chunkData.worldReference.GetBlockFromChunkCoordinates(chunkData, chunkData.worldPosition.x + x,
+            chunkData.worldPosition.y + y, chunkData.worldPosition.z + z);
+    }
+
+    public static Vector3Int GetBlockInChunkCoordinates(ChunkData chunkData, Vector3Int pos)
+    {
+        return new Vector3Int
+        {
+            x = pos.x - chunkData.worldPosition.x,
+            y = pos.y - chunkData.worldPosition.y,
+            z = pos.z - chunkData.worldPosition.z
+        };
+    }
+
+
+    public static MeshData GetChunkMeshData(ChunkData chunkData)
+    {
+        MeshData meshData = new MeshData(true);
+
+        LoopThroughTheBlocks(chunkData,(x,y,z) => 
+            BlockHelper.GetMeshData(chunkData, x, y, z, meshData, chunkData.blocks[GetIndexFromPosition(chunkData,x,y,z)]));
+
+
+        return meshData;
+    }
+
+    internal static Vector3Int ChunkPositionFromCoordinates(World world, int x, int y, int z)
+    {
+        Vector3Int pos = new Vector3Int
+        {
+            x = Mathf.FloorToInt(x / (float)world.chunkSize) * world.chunkSize,
+            y = Mathf.FloorToInt(y / (float)world.chunkHeight) * world.chunkHeight,
+            z = Mathf.FloorToInt(z / (float)world.chunkSize) * world.chunkSize
+        };
+        return pos;
+    }
+}
