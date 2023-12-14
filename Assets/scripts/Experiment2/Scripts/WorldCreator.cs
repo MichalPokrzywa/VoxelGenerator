84423b80674b315f78d291352ef02743b638d507
MichalPokrzywa <michpok467@student.polsl.pl>
2023-12-14 17:03:18 +0100

    added basic ui elements

--- Assets/scripts/Experiment2/Scripts/WorldCreator.cs
+++ Assets/scripts/Experiment2/Scripts/WorldCreator.cs
@@ -39,7 +39,7 @@ public class WorldCreator : MonoBehaviour
     public Slider loadingBar;
 
     public int drawRadius = 5;
-    //PerlinSettings
+    //PerlinSettings for generation
     public static PerlinSettings surfaceSettings;
     public PerlinGrapher surface;
 
@@ -59,6 +59,7 @@ public class WorldCreator : MonoBehaviour
     public HashSet<Vector2Int> ChunkColumns = new HashSet<Vector2Int>();
     public Dictionary<Vector3Int, ChunkBlock> chunks = new Dictionary<Vector3Int, ChunkBlock>();
 
+    public Material material;
     Vector3Int lastBuildPosition;
     public bool load = true;
     Queue<IEnumerator> buildQueue = new Queue<IEnumerator>();
@@ -79,6 +80,9 @@ public class WorldCreator : MonoBehaviour
     }
     void Start()
     {
+        //Material random = Instantiate(material);
+        //Debug.Log(random);
+        //random.color = Color.white;
         loadingBar.maxValue = worldDimensions.x  * worldDimensions.z;
         surfaceSettings = new PerlinSettings(surface.heightScale,surface.scale,surface.octaves,surface.heightOffset,surface.probability);
         stoneSettings = new PerlinSettings(stone.heightScale, stone.scale, stone.octaves, stone.heightOffset, stone.probability);
